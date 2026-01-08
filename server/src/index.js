675323b3fd213264b2f54290ba9c03797d90bda6
kaihere14 <yashpalsharma9950@gmail.com>
2026-01-08 11:51:10 +0530

    feat: enhance CORS configuration with additional options in index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,31 +1,38 @@
-import express from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import dotenv from 'dotenv';
-import connectDB from './db/connectDB.js';
-import fileRoutes from './routes/file.routes.js';
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import connectDB from "./db/connectDB.js";
+import fileRoutes from "./routes/file.routes.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({
+app.use(
+  cors({
     origin: ["https://drive.pawpick.store", "http://localhost:5173"],
-}));
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 
-app.use('/api/files', fileRoutes);
+app.use("/api/files", fileRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Discord Bot Server is running!');
+app.get("/", (req, res) => {
+  res.send("Discord Bot Server is running!");
 });
 
-connectDB().then(() => {
+connectDB()
+  .then(() => {
     app.listen(PORT, () => {
-        console.log(`Server is running on http://localhost:${PORT}`);
+      console.log(`Server is running on http://localhost:${PORT}`);
     });
-}).catch(err => {
-    console.error('Failed to connect to the database:', err);
-});
+  })
+  .catch((err) => {
+    console.error("Failed to connect to the database:", err);
+  });
